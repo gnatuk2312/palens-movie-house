8846283b20084a2587130d7a6dbaaabe2b18ad77
gnatuk2312 <84677177+gnatuk2312@users.noreply.github.com>
2022-01-27 23:44:02 +0200

    bug fix header + darkMode.js
    - Фікс transition при зникненні блока header__genres
    - Логотип в тег <a/>
    - Повністю робочий dark mode на сайті

--- src/modules/darkMode.js
+++ src/modules/darkMode.js
@@ -1,10 +1,9 @@
 export default class darkMode {
 	constructor(triggerSelector) {
 		this.trigger = document.querySelector(triggerSelector);
-		this.el1 = document.querySelector('.header__categories-menu');
-		this.els2 = document.querySelectorAll('.header__category');
-		this.el3 = document.querySelector('.header__button');
-
+		this.arrow = document.querySelector('.header__categories-menu .arrow');
+		//-------------
+		this.searchBtn = document.querySelector('.header__button');
 	}
 
 
@@ -12,45 +11,36 @@ export default class darkMode {
 	toggleTrigger() {
 		this.trigger.classList.toggle('is-dark')
 		if (this.trigger.classList.contains('is-dark')) {
-			this.trigger.children[1].classList.remove('visually-hidden');
-			this.trigger.children[0].classList.add('visually-hidden');
+			this.trigger.children[1].classList.remove('is-hidden');
+			this.trigger.children[0].classList.add('is-hidden');
 		} else {
-			this.trigger.children[1].classList.add('visually-hidden');
-			this.trigger.children[0].classList.remove('visually-hidden');
+			this.trigger.children[1].classList.add('is-hidden');
+			this.trigger.children[0].classList.remove('is-hidden');
 		}
 	}
 
-	changeColors() {
-		if (!this.trigger.classList.contains('is-dark')) {
-			//---------------------------
-			document.body.style.color = 'black';
-			//---------------------------
-			this.el1.children[1].classList.remove('dark-mode');
-			this.el1.children[1].classList.add('light-mode');
-			//---------------------------
-			this.els2.forEach(el => {
-				el.classList.remove('dark-mode');
-				el.classList.add('light-mode');
-			})
-			//---------------------------
-			this.el3.children[1].classList.remove('visually-hidden');
-			this.el3.children[0].classList.add('visually-hidden');
-		} else {
-			//---------------------------
-			document.body.style.color = 'white';
-			//---------------------------
-			this.el1.children[1].classList.add('dark-mode');
-			this.el1.children[1].classList.remove('light-mode');
-			//---------------------------
-			this.els2.forEach(el => {
-				el.classList.remove('light-mode');
-				el.classList.add('dark-mode');
-			})
-			//---------------------------
-			this.el3.children[1].classList.add('visually-hidden');
-			this.el3.children[0].classList.remove('visually-hidden');
-		}
+	toggleElement(element, selector) {
+		element = document.querySelector(selector).classList.toggle('dark-mode');
+	}
 
+	changeColors() {
+		this.toggleElement(this.header, '.header');
+		this.toggleElement(this.headerTitle, '.header__title');
+		this.toggleElement(this.headerInput, '.header__input');
+		this.toggleElement(this.genre, '.header__genre');
+		this.toggleElement(this.arrow, '.header__categories-menu .arrow');
+		this.toggleElement(this.movies, '.movies');
+		this.toggleElement(this.footer, '.footer');
+		this.toggleElement(this.footerRights, '.footer__rights');
+		this.toggleElement(this.footerMail, '.footer__mail');
+
+		if (!this.trigger.classList.contains('is-dark')) {
+			this.searchBtn.children[1].classList.remove('visually-hidden');
+			this.searchBtn.children[0].classList.add('visually-hidden');
+		} else {
+			this.searchBtn.children[0].classList.remove('visually-hidden');
+			this.searchBtn.children[1].classList.add('visually-hidden');
+		}
 	}
 
 	init() {
